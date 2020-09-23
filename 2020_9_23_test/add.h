80c73cf1915fecbd66b8bf8329f27e4c4b26328c
Abu-0425 <406307436@qq.com>
2020-09-23 23:59:44 +0800

    cpp_practice

---
+++ 2020_9_23_test/add.h
@@ -0,0 +1,3 @@
+
+
+int Add(int x, int y);//函数声明，若缺少，则找不到add.c/cpp中的Add函数
---
+++ 2020_9_23_test/main.cpp
@@ -0,0 +1,39 @@
+//#include <iostream>
+//using namespace std;
+
+#include "add.h"
+
+int main()
+{
+	Add(10, 20);
+	return 0;
+}
+
+/*
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+double Add(double x, double y)
+{
+	return x + y;
+}
+
+long Add(long x, long y)
+{
+	return x + y;
+}
+
+int main()
+{
+	int ret1 = Add(1, 2);
+	double ret2 = Add(1.23, 2.34);
+	long ret3 = Add(10L, 20L);
+	cout << "int: " << ret1 << endl;
+	cout << "double: " << ret2 << endl;
+	cout << "long: " << ret3 << endl;
+
+	return 0;
+}
+*/
