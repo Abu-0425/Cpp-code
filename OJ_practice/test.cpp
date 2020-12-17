72f169b50369450bd7e27b284bcc266942bd652a
Abu-0425 <406307436@qq.com>
2020-12-17 22:12:39 +0800

    add 扑克牌大小 + 相同的树 + 删除被覆盖的区间

--- OJ_practice/test.cpp
+++ OJ_practice/test.cpp
@@ -7,6 +7,91 @@
 #include <algorithm>
 using namespace std;
 
+#if 0
+//删除被覆盖的区间
+class Solution {
+public:
+	int removeCoveredIntervals(vector<vector<int>>& intervals) {
+		int size = intervals.size();
+		int cnt = size;
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				if(i != j && intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1]) {
+					cnt--;
+					break;
+				}
+			}
+		}
+		return cnt;
+	}
+};
+
+//扑克牌大小
+int main()
+{
+	string line;
+	while (getline(cin, line)){
+		//王炸最大，直接输出
+		if (line.find("joker JOKER") != -1) {
+			cout << "joker JOKER" << endl;
+		}
+		else {
+			int dash = line.find('-');
+			//分开两手牌
+			string car1 = line.substr(0, dash);
+			string car2 = line.substr(dash + 1);
+			//获取空格的次数，牌数为c + 1
+			int c1 = count(car1.begin(), car1.end(), ' ');
+			int c2 = count(car2.begin(), car2.end(), ' ');
+			//拿到第一张牌
+			string first1 = car1.substr(0, car1.find(' '));
+			string first2 = car2.substr(0, car2.find(' '));
+			string str = "345678910JQKA2jokerJOKER";
+			if (c1 == c2) {
+				//只要牌数相等，则第一张牌大的即为大
+				if (str.find(first1) > str.find(first2)) {
+					cout << car1 << endl;
+				}
+				else {
+					cout << car2 << endl;
+				}
+			}
+			else{
+				//牌数不相同，说明类型不同,只有炸弹可以和其它牌比较
+				//其它类型都是错误的
+				if (c1 == 3) {
+					cout << car1 << endl;
+				}
+				else if (c2 == 3) {
+					cout << car2 << endl;
+				}
+				else {
+					cout << "ERROR" << endl;
+				}
+			}
+		}
+	}
+}
+
+//相同的树
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (!p && !q) {
+			return true;
+		}
+		else if (!p || !q) {
+			return false;
+		}
+		else if (p->val != q->val) {
+			return false;
+		}
+		else {
+			return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+		}
+	}
+};
+
 //有序数组中出现次数超过25%的元素
 class Solution {
 public:
@@ -21,6 +106,7 @@ public:
 		return -1;
 	}
 };
+#endif
 
 
 #if 0
