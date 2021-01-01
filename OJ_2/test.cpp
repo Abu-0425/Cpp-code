15c37811833ef5bf46d52ba5ff74032f75d550be
Abu-0425 <406307436@qq.com>
2021-01-01 22:37:50 +0800

    在线OJ日常练习--整数反转

---
+++ OJ_2/test.cpp
@@ -0,0 +1,23 @@
+
+
+#if 0
+//整数反转
+class Solution {
+public:
+	int reverse(int x) {
+		int res = 0;
+		while (x) {
+			int pop = x % 10;
+			x /= 10;
+			if (res > INT_MAX / 10) {
+				return 0;
+			}
+			else if (res < INT_MIN / 10) {
+				return 0;
+			}
+			res = res * 10 + pop;
+		}
+		return res;
+	}
+};
+#endif
