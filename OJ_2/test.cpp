3a94cb3fe5ab0defbdd2ff38006d09b753d80dc0
Abu-0425 <406307436@qq.com>
2021-01-02 23:43:21 +0800

    add 三数之和

--- OJ_2/test.cpp
+++ OJ_2/test.cpp
@@ -1,4 +1,35 @@
 
+//三数之和
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		int size = nums.size();
+		sort(nums.begin(), nums.end());
+		vector<vector<int>> res;
+		for(int first = 0; first < size - 2; first++) {
+			if(first > 0 && nums[first] == nums[first - 1]) {
+				continue;
+			}
+			int third = size - 1;
+			int tatget = -nums[first];
+			for(int second = first + 1; second < size - 1; second++) {
+				if(second > first + 1 && nums[second] == nums[second - 1]) {
+					continue;
+				}
+				while(second < third && nums[second] + nums[third] > tatget) {
+					third--;
+				}
+				if(second == third) {
+					break;
+				}
+				if (nums[second] + nums[third] == tatget) {
+					res.push_back({ nums[first], nums[second], nums[third] });
+				}
+			}
+		}
+		return res;
+	}
+};
 
 #if 0
 //整数反转
