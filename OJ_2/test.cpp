77ef343c7730512f221cfde730cdd55edcbd9ced
Abu-0425 <406307436@qq.com>
2021-01-20 21:22:47 +0800

    add 洗牌

--- OJ_2/test.cpp
+++ OJ_2/test.cpp
@@ -5,6 +5,49 @@
 
 using namespace std;
 
+//洗牌
+vector<int> XiPai(vector <int> v, int n)
+{
+	vector<int> v1;
+	vector<int> v2;
+	for (int i = 0; i<n; i++)
+		v1.push_back(v[i]);     //左手中的牌
+	for (int i = n; i<2 * n; i++)
+		v2.push_back(v[i]);      //右手中的牌
+	for (int i = 0; i<n; i++)
+	{
+		v[i * 2] = v1[i];   //洗一次牌后左手牌的新位置
+		v[i * 2 + 1] = v2[i];    //洗一次牌后右手牌的新位置
+	}
+	return v;
+}
+
+int main()
+{
+	int T, n, k;
+	cin >> T;
+	while (T--)    //T组数据
+	{
+		vector<int> v;    
+		cin >> n >> k;
+		for (int i = 0; i < 2 * n; i++)
+		{
+			int a = 0;
+			cin >> a;
+			v.push_back(a);    //用数组存储牌号
+		}
+		while (k--)     //洗k次牌
+			v = XiPai(v, n);
+
+		for (int i = 0; i < 2 * n - 1; i++) {
+			cout << v[i] << " ";
+		}
+		cout << v[2 * n - 1] << endl;   //每组数据后要换行
+	}
+
+	return 0;
+}
+
 #if 0
 //最长公共子串的长度
 int getCommonStrLength(const string &s1, const string &s2)
